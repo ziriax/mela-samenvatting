e76f04e799cb3290c3ec58d6c74a17f21f404a5a
Peter Verswyvelen <bugfact@gmail.com>
2025-04-23 18:38:13 +0200

    Samenvatting projectie matrices + eerste oplossing examenvraag

--- to_pdf.sh
+++ to_pdf.sh
@@ -2,8 +2,12 @@
 SCRIPT_DIR="$(dirname "$BASH_SOURCE")"
 pushd "$SCRIPT_DIR"
 rm -f mela.pdf
+rm -f examens.pdf
 mkdir -p _out
 find _out -name "*.pdf" -delete
-find src -name "*.tex" -exec pdflatex  -interaction nonstopmode -file-line-error -output-directory _out {} \;
+find src/theorie -name "*.tex" -exec pdflatex  -interaction nonstopmode -file-line-error -output-directory _out {} \;
 pdfunite _out/*.pdf mela.pdf
+find _out -name "*.pdf" -delete
+find src/examens -name "*.tex" -exec pdflatex  -interaction nonstopmode -file-line-error -output-directory _out {} \;
+pdfunite _out/*.pdf examens.pdf
 popd
